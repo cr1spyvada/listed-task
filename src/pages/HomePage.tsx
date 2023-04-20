70671ae30e8471cbbeae78f7b6e1f1e23274a713
vaisakh-rivi <vaisakh@rivi.co>
2023-04-21 01:12:38 +0530

    feat: home page itr -1

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,42 @@ export const HomePage = () => {
   return (
     <div className="flex h-full">
       <div className="flex-[2] flex text-7xl font-bold text-center justify-center items-center">
-        Board
+        Board.
       </div>
-      <div className="flex-[3] bg-white flex justify-center items-center text-black">
-        <div className="w-1/2">
+      <div className="flex-[3] bg-slate-200 flex justify-center items-center text-black">
+        <div className="w-[50%] flex flex-col gap-y-2">
           <div className="font-bold text-4xl">Sign In</div>
           <div className="text-base">Sign into your account</div>
-          <button className="rounded-lg"></button>
+          <div className="flex justify-between gap-2">
+            <button className="flex-[1] rounded-md text-stone-500 p-2 bg-white">
+              Sign in with Google
+            </button>
+            <button className="flex-[1] rounded-md text-stone-500 p-2 bg-white">
+              Sign in with Apple
+            </button>
+          </div>
+          <div className="bg-white flex flex-col p-4 gap-2 rounded-xl">
+            Email address
+            <input
+              type="text"
+              className="bg-stone-300 rounded-lg p-2"
+              defaultValue="johndoe@gmail.com"
+            />
+            Password
+            <input
+              type="password"
+              className="bg-stone-300 rounded-lg p-2"
+              defaultValue="12345678"
+            />
+            <div className="text-blue-500">Forgot password?</div>
+            <button className="rounded-lg bg-black text-white p-2">
+              Sign In
+            </button>
+          </div>
+          <div className="w-full text-center flex justify-center">
+            Don't have an account?{" "}
+            <div className="text-blue-300"> Register here</div>
+          </div>
         </div>
       </div>
     </div>
